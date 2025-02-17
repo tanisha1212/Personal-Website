6ee264d079b4f8eddca6d0f09f0a8fb879e01d5f
tanishatazreen <122285012+tanisha1212@users.noreply.github.com>
2025-02-16 21:28:17 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    const toggleBtn = document.querySelector('.toggle-btn');
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = 'Toggle Light Mode';
+    } else {
+        toggleBtn.textContent = 'Toggle Dark Mode';
+    }
+}
